ab9cee217134674bc9e336802628ffa9f93d48a8
Pelle Ravn <pelle@pelleravn.dk>
2019-10-17 17:54:58 +0200

    Added (mostly) empty implementations of known endpoints from Arlo

--- Arlo/ArloClient.cs
+++ Arlo/ArloClient.cs
@@ -48,10 +48,97 @@ namespace Arlo
             return result;
         }
 
+        public async Task<Result<List<Media>>> ArmDeviceAsync(Device device)
+        {
+            var notifyRequest = new NotifyRequest(device, "USER_ID_web", Mode.Arm);
+            //        {
+            //            "from":    "3AB3-210-6687469_web",
+            //"to":    "48E46573A0B8B",
+            //"action":    "set",
+            //"resource":    "modes",
+            //"transId":    "web!3975ac7b.ebb3a8!1504266382584",
+            //"publishResponse":    true,
+            //"properties": {
+            //                "active":    "mode1"
+            //}
+            //        }
+            var result = await _requestHandler.PostAsync<List<Media>>("hmsweb/users/library", notifyRequest);
+
+            return result;
+        }
+
+        public async Task<Result<List<Media>>> DisarmDeviceAsync(Device device)
+        {
+            var notifyRequest = new NotifyRequest(device, "USER_ID_web", Mode.Disarm);
+            //        {
+            //            "from":    "3AB3-210-6687469_web",
+            //"to":    "48E46573A0B8B",
+            //"action":    "set",
+            //"resource":    "modes",
+            //"transId":    "web!3975ac7b.ebb3a8!1504266382584",
+            //"publishResponse":    true,
+            //"properties": {
+            //                "active":    "mode0"
+            //}
+            //        }
+            var result = await _requestHandler.PostAsync<List<Media>>("hmsweb/users/library", notifyRequest);
+
+            return result;
+        }
+
         public async Task<Result<List<Media>>> GetLibraryAsync(DateTime dateFrom, DateTime dateTo)
         {
             var mediaRequest = new MediaLibraryRequest(dateFrom, dateTo);
             var result = await _requestHandler.PostAsync<List<Media>>("hmsweb/users/library", mediaRequest);
+
+            return result;
+        }
+
+        public async Task<Result<List<object>>> GetLibraryMetadataAsync()
+        {
+            var result = await _requestHandler.GetAsync<List<object>>("hmsweb/users/library/metadata/v2");
+
+            return result;
+        }
+
+        public async Task<Result<List<object>>> GetProfileAsync()
+        {
+            var result = await _requestHandler.GetAsync<List<object>>("hmsweb/users/profile");
+
+            return result;
+        }
+
+        public async Task<Result<object>> GetSessionAsync()
+        {
+            var result = await _requestHandler.GetAsync<object>("hmsweb/users/session");
+
+            return result;
+        }
+
+        public async Task<Result<List<object>>> GetFriendsAsync()
+        {
+            var result = await _requestHandler.GetAsync<List<object>>("hmsweb/users/friends");
+
+            return result;
+        }
+
+        public async Task<Result<List<object>>> GetUserLocationsAsync()
+        {
+            var result = await _requestHandler.GetAsync<List<object>>("hmsweb/users/locations");
+
+            return result;
+        }
+
+        public async Task<Result<List<object>>> GetServiceLevelAsync()
+        {
+            var result = await _requestHandler.GetAsync<List<object>>("hmsweb/users/serviceLevel/v2");
+
+            return result;
+        }
+
+        public async Task<Result<List<object>>> GetPaymentOffersAsync()
+        {
+            var result = await _requestHandler.GetAsync<List<object>>("hmsweb/users/payment/offers");
 
             return result;
         }
---
+++ Arlo/Models/Requests/NotifyRequest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Arlo.Models.Requests
+{
+    public enum Mode
+    {
+        Disarm,
+        Arm
+    }
+
+    public class NotifyRequest
+    {
+        public NotifyRequest(Device device, string from, Mode mode)
+        {
+            To = device.DeviceId;
+            From = from;
+            Properties = new NotifyProperties(mode);
+        }
+
+        public NotifyRequest()
+        {
+
+        }
+
+        public class NotifyProperties
+        {
+            public NotifyProperties(Mode mode)
+            {
+                switch (mode)
+                {
+                    case Mode.Disarm:
+                        Active = "mode0";
+                        break;
+                    case Mode.Arm:
+                        Active = "mode1";
+                        break;
+                    default:
+                        throw new ArgumentException("Unknown mode!");
+                }
+            }
+
+            public NotifyProperties(string active)
+            {
+                Active = active;
+            }
+
+            public NotifyProperties()
+            {
+
+            }
+
+            [JsonPropertyName("active")]
+            public string Active { get; set; }
+        }
+
+        [JsonPropertyName("from")]
+        public string From { get; set; }
+
+        [JsonPropertyName("to")]
+        public string To { get; set; }
+
+        [JsonPropertyName("action")]
+        public string Action { get; set; } = "set";
+
+        [JsonPropertyName("resource")]
+        public string Resource { get; set; } = "modes";
+
+        [JsonPropertyName("transId")]
+        public string TransId { get; set; }
+
+        [JsonPropertyName("publishResponse")]
+        public bool PublishResponse { get; set; } = true;
+
+        [JsonPropertyName("properties")]
+        public NotifyProperties Properties { get; set; } = new NotifyProperties();
+    }
+}
